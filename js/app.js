276e9da85645266f7e814c54384a8f74488b866d
PMagombedze <percymagoras@outlook.com>
2024-08-26 16:20:50 +0200

    modify site icon

--- js/app.js
+++ js/app.js
@@ -6,6 +6,14 @@ document.querySelector(".hamburger").addEventListener("click", () => {
 });
 
 /**
+ * send request
+ */
+function SendRequest() {
+  // onclick, change location to /pages/en-us/contact.html
+  window.location.href = "./pages/en-us/contact.html";
+}
+
+/**
  * partners slideshow
  */
 let slideIndex = 0;
